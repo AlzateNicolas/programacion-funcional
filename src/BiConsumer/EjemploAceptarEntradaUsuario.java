0c85ba056261b3ab89ef94a3a24d5529f2b4a803
AlzateNico <nalzate@cue.edu.co>
2024-05-21 16:13:51 -0500

    si

---
+++ src/BiConsumer/EjemploAceptarEntradaUsuario.java
@@ -0,0 +1,22 @@
+package BiConsumer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EjemploAceptarEntradaUsuario {
+
+    public static void main(String[] args) throws IOException {
+        // Crear BufferedReader para leer la entrada del usuario
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+
+        // Aceptar entrada del usuario
+        System.out.print("Ingrese su nombre: ");
+        String nombre = lector.readLine();
+
+        // Mostrar el nombre ingresado
+        System.out.println("¡Hola " + nombre + "!");
+
+        // Cerrar BufferedReader
+        lector.close();
+    }
+}
